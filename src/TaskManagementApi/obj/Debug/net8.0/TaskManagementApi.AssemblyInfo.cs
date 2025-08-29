12fd0cfb601b69418b914886bb69856485174449
Nouman Ali <noomiali@hotmail.com>
2025-08-30 01:03:58 +0200

    config updates

--- src/TaskManagementApi/obj/Debug/net8.0/TaskManagementApi.AssemblyInfo.cs
+++ src/TaskManagementApi/obj/Debug/net8.0/TaskManagementApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagementApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+554abf295baab29135c1edfa1f9fa4b1ce207033")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagementApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagementApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
